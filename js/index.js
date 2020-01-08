db79a09e2d5c5a4c1ca43268320ac73071ef9af9
WatingChen <1293223859@qq.com>
2020-01-08 17:58:42 +0800

    查看详情未完成

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // 轮播图
 $("#banner").banner({
     img:["/img/banner.jpg","/img/banner1.jpg","/img/banner2.jpg","/img/banner3.jpg","/img/banner4.jpg","/img/banner5.jpg"],
-    btn:true,
+    btn:false,
     list:true,
     autoPlay:true,
     moveTime:200,
