54380953b4cda3c9edc26781e33d2551929a898f
WatingChen <1293223859@qq.com>
2020-01-09 16:09:06 +0800

    周四下午四点之前的最后一个版本

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // 轮播图
 $("#banner").banner({
     img:["/img/banner.jpg","/img/banner1.jpg","/img/banner2.jpg","/img/banner3.jpg","/img/banner4.jpg","/img/banner5.jpg"],
-    btn:false,
+    btn:true,
     list:true,
     autoPlay:true,
     moveTime:200,
